3a159ec3487f7dfb6ff797aa77ef114d8f2bd932
0hashi <pohashi@gmail.com>
2025-01-20 09:05:38 -0600

    Update AdministrativePS-Commands.ps1

--- AdministrativePS-Commands.ps1
+++ AdministrativePS-Commands.ps1
@@ -54,16 +54,15 @@ qwinsta /server:172.18.1.4 | Where-Object {$_ -notmatch "SYSTEM"}
 Get-EventLog -LogName Security | Where-Object {$_.EventID -eq 4624} | Select-Object -Property @{Name='TimeGenerated';Expression={$_.TimeGenerated}}, @{Name='User';Expression={$_.ReplacementStrings[5]}} | Sort-Object TimeGenerated -Descending | Where-Object {$_.TimeGenerated -ge (Get-Date).AddDays(-5)} | Where-Object {$_ -notmatch "SYSTEM"}
 
 #--------------------------------------------------------------------
+# Get a list of AD users in the Texas OU
+Get-ADUser -Filter * -SearchBase "OU=Texas,DC=verticalcable,DC=local" | Select-Object Name, DistinguishedName | Sort-Object Name
+
+#--------------------------------------------------------------------
 
 #PO - Display the number of disabled user accounts.
 Search-ADAccount -AccountDisabled -UsersOnly -SearchBase "OU=Texas,DC=verticalcable,DC=local" | FT Name, DistinguishedName | Measure-Object -Line
 #PO - Display the disabled user accounts.
-Search-ADAccount -AccountDisabled -UsersOnly -SearchBase "OU=Texas,DC=verticalcable,DC=local" | FT Name, DistinguishedName
-
-#--------------------------------------------------------------------
-
-#PO - Execute remote PowerShell cmd
-Invoke-Command -ComputerName design1 -ScriptBlock { Get-LocalGroupMember -Group $GroupName }
+Search-ADAccount -AccountDisabled -UsersOnly -SearchBase "OU=Texas,DC=verticalcable,DC=local" | Sort-Object -Property Name | FT Name, DistinguishedName 
 
 #--------------------------------------------------------------------
 
@@ -73,6 +72,7 @@ Get-AzureADSubscribedSku
 #--------------------------------------------------------------------
 
 #PO - Get a list of users and their assigned license SKUs
+Connect-AzureAD
 Get-AzureADUser -All $true | ForEach-Object {
     $user = $_
 
@@ -108,6 +108,10 @@ foreach ($Computer in $ComputerNames) {
 
 #--------------------------------------------------------------------
 
+#PO - Execute remote PowerShell cmd
+Invoke-Command -ComputerName design1 -ScriptBlock { Get-LocalGroupMember -Group $GroupName }
+
+#--------------------------------------------------------------------
 # Shutdown Windows with WMIC
 Invoke-Command -ComputerName computername -ScriptBlock { shutdown /s /t 0 }
 
@@ -148,3 +152,95 @@ $Duplicates | ForEach-Object {
 #--------------------------------------------------------------------
 
 Get-DhcpServerv4Reservation -ComputerName "10.100.104.7" -ScopeId 10.100.104.0
+
+#--------------------------------------------------------------------
+
+# Find users in specific privileged groups and print the results to STDOOUT and if selected
+# to the default printer
+$privilegedGroups = "Domain Admins", "Enterprise Admins", "Schema Admins", "Administrators"
+$privilegedAccts = foreach ($group in $privilegedGroups) {
+    Get-ADGroupMember -Identity $group | Select-Object Name, SamAccountName, DistinguishedName,@{Name='Group';Expression={$group}}
+}
+$privilegedAccts = $privilegedAccts | Sort-Object -Property Name 
+
+# Output and print the results
+Write-Output $privilegedAccts | Format-Table -AutoSize
+
+###$privilegedAccts | Export-Csv -Path "TCI_PrivilegedAccounts.csv" -NoTypeInformation
+###Out-Printer -InputObject $privilegedAccts
+
+#--------------------------------------------------------------------
+
+# Find users with adminCount attribute set to 1
+Get-ADUser -Filter "adminCount -eq 1" | Select-Object Name, SamAccountName, Enabled | Sort-Object -Property Name
+
+#--------------------------------------------------------------------
+
+# Find users with specific rights (e.g., reset password)
+Get-ADObject -Filter 'objectClass -eq "user"' -Properties userAccountControl | 
+    Where-Object {$_.userAccountControl -band 0x10000} | 
+    Select-Object Name, SamAccountName, Enabled |Sort-Object -Property Name
+
+#--------------------------------------------------------------------
+
+# Find disabled users in OU and move to another OU
+# Specify the source and destination OUs
+$sourceOU = "OU=Texas,DC=verticalcable,DC=local"
+$destinationOU = "OU=Disabled Users,OU=Texas,DC=verticalcable,DC=local"
+
+# Get all disabled users in the source OU
+$disabledUsers = Get-ADUser -SearchBase $sourceOU -Filter {Enabled -eq $false}
+
+# Move each disabled user to the destination OU
+foreach ($user in $disabledUsers) {
+    #Move-ADObject -Identity $user.DistinguishedName -TargetPath $destinationOU
+    Write-Output $user "moved to " $destinationOU
+    Write-Output "---------------------------------"
+}
+
+# Get the Group Membership of a user object
+Get-ADPrincipalGroupMembership lead.operator | Select-Object name
+Get-ADPrincipalGroupMembership -Identity lead.operator | Get-ADGroupMember
+
+
+
+$username = "lead.operator"  # Replace with the actual username
+$userGroups = Get-ADPrincipalGroupMembership -Identity $username  
+
+foreach ($group in $userGroups) {
+    Write-Host "Group: $($group.Name)"
+        $nestedGroups = Get-ADGroupMember -Identity $group.Name | Where-Object { $_.ObjectClass -eq "group" }
+            foreach ($nestedGroup in $nestedGroups) {
+                    Write-Host "  - $($nestedGroup.Name)"
+    }
+}
+
+#--------------------------------------------------------------------
+$user = "lead.operator"
+$groups = Get-ADPrincipalGroupMembership -Identity $user 
+
+
+# Get the members of each group
+foreach ($group in $groups) {
+    Write-Host "Members of group $($group.Name):"
+    Get-ADGroupMember -Identity $group.Name
+}
+
+#--------------------------------------------------------------------
+#PO - Get group membership of user
+Get-ADPrincipalGroupMembership -Identity "lead.operator" | Select-Object name
+
+#--------------------------------------------------------------------
+
+$User = "lead.operator"
+
+
+$Groups = Get-ADUser -Identity $User | Get-ADPrincipalGroupMembership
+$Groups | ForEach-Object {
+    Get-ADGroup -Identity $_.DistinguishedName -Properties MemberOf
+} | Select Name
+
+Get-ADUser -Identity $User | Get-ADPrincipalGroupMembership | Select-Object Name
+
+
+Get-ADUser -Filter { extensionAttribute1 -notlike "*" } -Properties extensionAttribute1
